4e5742e64ddba9f0c24b3633dae1ef7823bbc30e
Henrry Bourgeot <henrrybrgt@gmail.com>
2022-08-20 14:35:50 -0400

    Added JSON Manipulation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -12,4 +13,35 @@ func HandleRoot(w http.ResponseWriter, r *http.Request) {
 func HandleHome(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "This is the API endpoint")
+}
+
+func PostRequest(w http.ResponseWriter, r *http.Request) {
+	var metadata MetaData
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&metadata)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "Payload %v\n", metadata)
+}
+
+func UserPostRequest(w http.ResponseWriter, r *http.Request) {
+	var user User
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+
+	res, err := user.ToJSON()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,6 +12,9 @@ func main() {
 	server := NewServer(":4000")
 	server.Handle("GET", "/", HandleRoot)
 	server.Handle("POST", "/api", server.AddMiddleware(HandleHome, CheckAuth(), Loggin()))
+	server.Handle("POST", "/create", PostRequest)
+	server.Handle("POST", "/user", UserPostRequest)
+
 	err := server.Listen()
 	isAnError(err)
 
